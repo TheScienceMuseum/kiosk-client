558a50d1c40744e9860d53a98c0805767e916e54
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-09 10:08:50 +0100

    adds bootstrap

--- src/ui/src/App.js
+++ src/ui/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
@@ -8,17 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <small className={'text-muted'}>
+            CONF 0001
+          </small>
         </header>
       </div>
     );
