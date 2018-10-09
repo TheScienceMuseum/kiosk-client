71450ce6a6caebbe87869331bcf7bf81fe446c10
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2018-10-09 10:04:04 +0100

    adds react frontend with development server

--- src/index.js
+++ src/index.js
@@ -6,22 +6,42 @@ if (updateIsRunning) {
 }
 
 let mainWindow;
+const applicationDevMode = process.env.APPLICATION_ENV && process.env.APPLICATION_ENV === 'dev';
 
 const onApplicationStart = () => {
-  mainWindow = new BrowserWindow({
+  const windowOptions = {
     resizable: false,
     movable: false,
     minimizable: false,
     closable: false,
     alwaysOnTop: true,
-    // fullscreen: true,
-  });
+    fullscreen: true,
+  };
 
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  if (applicationDevMode) {
+    windowOptions.width = 1920 / 2;
+    windowOptions.height = 1200 / 2;
+    windowOptions.resizable = true;
+    windowOptions.movable = true;
+    windowOptions.minimizable = true;
+    windowOptions.closable = true;
+    windowOptions.alwaysOnTop = false;
+    windowOptions.fullscreen = false;
+  }
 
-  mainWindow.webContents.openDevTools({
-    detach: true,
-  });
+  mainWindow = new BrowserWindow(windowOptions);
+
+  mainWindow.loadURL(
+    (applicationDevMode) ?
+      'http://localhost:3000' :
+      `file://${__dirname}/index.html`,
+  );
+
+  if (applicationDevMode) {
+    mainWindow.webContents.openDevTools({
+      detach: true,
+    });
+  }
 
   mainWindow.on('closed', () => {
     mainWindow = null;
