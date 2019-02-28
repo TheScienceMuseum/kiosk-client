d2e856f70665031d80d30f9f832c4fb18913d0ad
Lawrence Goldstien <lawrence.goldstien@joipolloi.com>
2019-02-28 10:05:14 +0000

    add staging default environment for building

--- build.sh
+++ build.sh
@@ -2,13 +2,17 @@
 
 S3_BUCKET="joipolloi-scimus-builds"
 AWS_PROFILE="scimus"
+ENVIRONMENT=$1
+
+if [[ -z "$ENVIRONMENT" ]]; then ENVIRONMENT='staging'; fi
+
 
 echo "Building for OSX"
 echo
 echo "You must have aws cli tools, yarn, and node installed"
 echo
 yarn install
-yarn dist:staging:mac
+yarn dist:${ENVIRONMENT}:mac
 cd dist
 ARCHIVE_NAME=`echo *.pkg | sed "s/.pkg/.zip/"`
 zip ${ARCHIVE_NAME} *.pkg
