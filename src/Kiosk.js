acea099acd8bbf2d0ec5fc9fe597974fc5d2d839
Matthew Moore <matthew.moore@joipolloi.com>
2019-10-30 10:56:21 +0000

    Timeout increased instead to compensate for long downloads

--- src/Kiosk.js
+++ src/Kiosk.js
@@ -14,7 +14,6 @@ class Kiosk {
   constructor() {
     this.window = new Window('main');
     this.packageManager = new PackageManager();
-    this.downloading = false;
     this.window_debug = null;
     this.currently_displayed_package = this.packageManager.getCurrentPackage();
 
@@ -130,21 +129,16 @@ class Kiosk {
           if (foundPackage) {
             this.updateDisplayedPackage(foundPackage);
           } else {
-            if(!this.downloading)
-            {
-            this.downloading = true;
             newPackage.download(_.get(packageData, 'path'))
-              .then(() => {
-                this.updateDisplayedPackage(newPackage);
-              });
-            }
+            .then(() => {
+              this.updateDisplayedPackage(newPackage);
+            });
           }
         }
       });
   }
 
   updateDisplayedPackage(newPackage) {
-    this.downloading = false;
     this.packageManager.rebuildPackageCache();
     const currentPackage = this.packageManager.getCurrentPackage();
     let canUpdate = true;
